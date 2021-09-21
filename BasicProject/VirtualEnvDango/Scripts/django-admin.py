7ab19ab92d2a5e163de1d0a1905aa9163326b438
joaop <joaop@LAPTOP-JNIA2EKC>
2021-09-21 20:47:50 -0300

    Adicionar arquivos de projeto.

---
+++ BasicProject/VirtualEnvDango/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\joaop\Documents\Python Tutorial Django\LearningDjango\BasicProject\VirtualEnvDango\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
